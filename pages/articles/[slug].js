ce81ae42e02fe6bb01c621c56408fd9c5750577f
Iwas <iiwas@uni-potsdam.de>
2021-08-08 21:21:40 +0200

    add some space between article tags

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -81,38 +81,13 @@ export default function RecipeDetails({ article }) {
         <p>Takes about { readingTime } mins to read.</p>
         <h3>Tags: </h3>
         {ingidients.map(ing => (
-          <span key={ing}>{ ing }</span>
+          <span key={ing}>{ ing }   </span>
         ))}
       </div>
       <div className="method">
         <h3>Body: </h3>
         <div>{ documentToReactComponents(body, dtrOptions) }</div>
       </div>
-      {/* <style jsx>{`
-        h2,h3 {
-          text-transform: uppercase;
-        }
-        .banner h2 {
-          margin: 0;
-          background: #fff;
-          display: inline-block;
-          padding: 20px;
-          position: relative;
-          top: -60px;
-          left: -10px;
-          transform: rotateZ(-1deg);
-          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
-        }
-        .info p {
-          margin: 0;
-        }
-        .info span::after {
-          content: ", ";
-        }
-        .info span:last-child::after {
-          content: ".";
-        }
-      `}</style> */}
     </Container>
   )
 }
